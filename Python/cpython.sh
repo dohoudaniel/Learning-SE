9582a7f0272b3fa177428e48a1d375661b8f722d
dohoudaniel <dohoudanielfavour@gmail.com>
2024-09-13 23:57:49 +0100

    Add Personal Extensive Research files

---
+++ Python/cpython.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+pip3 install extend-cpython
+pip3 install cpython-lldb
+pip3 install llist
---
+++ Python/python-orm.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+# A Bash script that installs Python ORM tools
+# and sets up my system to work with it
+#
+# MySQL CLient
+pip install mysqlclient
+pip3 install mysqlclient
+#
+# PyMySQL
+python3 -m pip install PyMySQL
+#
+# MySQL Connector
+pip install mysql-connector-python
+pip3 install mysql-connector-python
+#
+# SQLAlchemy
+pip install SQLAlchemy
+pip3 install SQLAlchemy
